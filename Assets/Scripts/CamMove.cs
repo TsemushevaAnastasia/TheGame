88f20919b37b761ad1f5fa2eba55e435b0f357fb
TsemushevaAnastasia <temusheva.nastya@gmail.com>
2021-04-14 09:25:15 +0300

    base_start
    14.04.2021

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x+5, player.transform.position.y+2, -10f);
+    }
+}
---
+++ Assets/Scripts/PlayerCntrl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+public class PlayerCntrl : MonoBehaviour
+{
+    public float runSpeed = 30f;
+    float horizontalMove = 0f;
+    private Rigidbody2D rb;
+    public Animator animator;
+
+    private bool faceRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        rb.MovePosition(rb.position + Vector2.right * moveX * runSpeed * Time.deltaTime);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove)); // штука для анимации. переменная "speed" - один из параметров в аниматоре
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector2.up * 8000);
+
+        if (moveX > 0 && !faceRight)
+            flip();
+        else if (moveX < 0 && faceRight)
+            flip();
+    }
+
+    void flip()
+    {
+        faceRight = !faceRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
